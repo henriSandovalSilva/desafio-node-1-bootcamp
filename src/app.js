33e8379711e05a75e61222c790fc3f778176a07f
Henri <henri.silvatj@hotmail.com>
2020-07-18 16:55:23 -0300

    Implementado CRUD de repositorios; Implementado like; Finalizando o desafio;

--- src/app.js
+++ src/app.js
@@ -1,33 +1,124 @@
-const express = require("express");
-const cors = require("cors");
+const express = require("express")
+const cors = require("cors")
 
-// const { uuid } = require("uuidv4");
+const { uuid } = require("uuidv4")
 
-const app = express();
+const app = express()
 
-app.use(express.json());
-app.use(cors());
+app.use(express.json())
+app.use(cors())
 
-const repositories = [];
+const repositories = []
 
+/**
+ * Lista todos os repositórios
+ */
 app.get("/repositories", (request, response) => {
-  // TODO
-});
+  return response.json(repositories)
+})
 
+/**
+ * Cria um repositório
+ */
 app.post("/repositories", (request, response) => {
-  // TODO
-});
+  const { title, url, techs } = request.body
 
+  data = {
+    id: uuid(),
+    title,
+    url,
+    techs,
+    likes: 0
+  }
+
+  repositories.push(data)
+
+  return response.json(data)
+})
+
+/**
+ * Atualiza um repositório
+ */
 app.put("/repositories/:id", (request, response) => {
-  // TODO
-});
 
+  const { id } = request.params
+
+  const { title, url, techs } = request.body
+
+  const repositoryIndex = repositories.findIndex(e => e.id === id)
+
+  /**
+   * Verifica se o repositório existe
+   */
+  if (repositoryIndex >= 0) {
+    /**
+     * Atualiza o repositório
+     */
+    repositories[repositoryIndex] = {
+      id,
+      title,
+      url,
+      techs,
+      likes: repositories[repositoryIndex].likes
+    }
+  
+    return response.json(repositories[repositoryIndex])
+  }
+
+
+  return response.status(400).json({
+    error: "Repositório não encontrado."
+  })
+})
+
+/**
+ * Remove um repositório
+ */
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
-});
+  const { id } = request.params
 
+  const repositoryIndex = repositories.findIndex(e => e.id === id)
+
+  /**
+   * Verifica se o repositório existe
+   */
+  if (repositoryIndex >= 0) {
+    /**
+     * Remove o repositório do array
+     */
+    repositories.splice(repositoryIndex, 1)
+
+    return response.status(204).json({})
+  }
+
+  return response.status(400).json({
+    error: "Repositório não encontrado."
+  })
+})
+
+/**
+ * Dá like em um repositório
+ */
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
-});
+  const { id } = request.params
+
+  const repositoryIndex = repositories.findIndex(e => e.id === id)
+
+  /**
+   * Verifica se o repositório existe
+   */
+  if (repositoryIndex >= 0) {
+    /**
+     * Aumenta o numero de likes
+     */
+    repositories[repositoryIndex].likes++;
+
+    return response.json(repositories[repositoryIndex])
+  }
+
+  return response.status(400).json({
+    error: "Repositório não encontrado."
+  })
+})
 
 module.exports = app;
